d5fa2410210e7f881c2c2786885b6b666fb7f204
samu-rize <6710@holbertonstudents.com>
2023-09-12 17:34:08 +0100

    add nop & swap & add

--- op_functions_2.c
+++ op_functions_2.c
@@ -9,7 +9,7 @@ void pint(stack_t **stack, unsigned int line)
 {
 	if (*stack == NULL || stack == NULL)
 	{
-        error_exit_d("L%u: can't pint, stack empty\n", line, EXIT_FAILURE);
+		error_exit_d("L%u: can't pint, stack empty\n", line, EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
 }
@@ -47,4 +47,43 @@ void nop(stack_t **stack, unsigned int line)
 {
 	(void)line;
 	(void)stack;
+}
+/**
+ * swap -swap the top two elements of the stack.
+ * @stack: pointer to the top of the stack.
+ * @line_number: current number in the script.
+ *
+ * Return: void.
+*/
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		error_exit_d("L%u: can't swap, stack too short\n",
+		line_number, EXIT_FAILURE);
+	}
+	temp = (*stack)->next;
+	(*stack)->next = temp->next;
+	temp->next = (*stack);
+	temp->prev = NULL;
+	(*stack)->prev = temp;
+	(*stack) = temp;
+}
+/**
+ * add -adds the top two elements of the stack.
+ * @stack: pointer to the top of the stack.
+ * @line_number: current number in the script.
+ *
+ * Return: void.
+*/
+void add(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		error_exit_d("L%u: can't add, stack too short\n", line_number, EXIT_FAILURE);
+	}
+	(*stack)->next->n += (*stack)->n;
+	pop(stack, line_number);
 }
