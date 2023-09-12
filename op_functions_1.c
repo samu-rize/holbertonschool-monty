f3089f3316a862f63dabb2f8e7508bac6353d09d
samu-rize <6710@holbertonstudents.com>
2023-09-12 12:08:52 +0100

    push and pall functions

---
+++ op_functions_1.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+/**
+ * push - push element in the stack.
+ * @stack: the stack
+ * @line: line number.
+ * Return: void
+*/
+void push(stack_t **stack, unsigned int line)
+{
+	(void)line;
+	int n;
+
+	add_dnodeint(stack, n);
+
+}
+
+/**
+ * pall - print all element of the stack.
+ * @stack: the stack
+ * @line: line number.
+ * Return: void
+*/
+void pall(stack_t **stack, unsigned int line)
+{
+	(void)line;
+
+	if (*stack == NULL)
+		return;
+	stack_t *current = *stack;
+
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
